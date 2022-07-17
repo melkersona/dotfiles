e1038928e0126429b9aa91e66824b58d9e7bc485
Alva Melkerson <alva.melkerson@gmail.com>
2022-07-17 14:10:03 -0500

    added system tray to left-side monitor (DP-0)

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,6 +5,8 @@ killall -q polybar
 
 for m in $(polybar -m | cut -d ":" -f1); do
 	for d in $MODULES; do
-		MONITOR=$m polybar -r $d &
+		MONITOR=$m polybar -l info -r $d &
 	done
 done
+
+MONITOR=DP-0 polybar -r tray
